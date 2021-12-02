785c12e78d34390bc59debf0dd5d9fabffb987b0
Burak <burakinan1@hotmail.com>
2021-12-02 16:15:05 +0100

    Use Kotlin collections functions

--- day1/src/dev/burak/aoc/day1/SonarSweep.kt
+++ day1/src/dev/burak/aoc/day1/SonarSweep.kt
@@ -13,36 +13,14 @@ fun main() {
 
 fun part1(dataSet: List<Int>) {
     println(dataSet
-        .filterIndexed { i, value -> i != 0 && dataSet[i - 1] < value }
-        .count()
+        .windowed(2)
+        .count { (prev, cur) -> prev < cur }
     )
 }
 
 fun part2(dataSet: List<Int>) {
-    var count = 0
-    for (i in dataSet.indices) {
-        if (i == 0) {
-            continue
-        }
-
-        val curSum = getSum(dataSet, i)
-        val prevSum = getSum(dataSet, i - 1)
-        if (curSum == Int.MAX_VALUE || prevSum == Int.MAX_VALUE) {
-            break
-        }
-
-        if (curSum > prevSum) {
-            count++
-        }
-    }
-
-    println(count)
-}
-
-fun getSum(dataSet: List<Int>, index: Int): Int {
-    if (index + 2 > dataSet.lastIndex) {
-        return Int.MAX_VALUE
-    }
-
-    return dataSet[index] + dataSet[index + 1] + dataSet[index + 2]
+    println(dataSet
+        .windowed(4)
+        .count { it.first() < it.last() }
+    )
 }
